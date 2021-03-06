8c4661fe6019f6d7d1cd4615098a51fb4bc15882
ebisLab <ebaiden@techie.com>
2021-03-06 10:54:50 -0500

    stripe input style

---
+++ src/styles/stripestyle.js
@@ -0,0 +1,22 @@
+export const style = {
+    base: {
+        iconColor: '#c4f0ff',
+        color: '#c4f0ff',
+        fontWeight: '500',
+        fontFamily: 'Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+          color: '#fce883',
+        },
+        '::placeholder': {
+          color: '#87BBFD',
+        },
+      },
+    invalid: {
+      color: '#e5424d',
+      ':focus': {
+        color: '#303238',
+      },
+    },
+  };
